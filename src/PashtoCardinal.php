1048203e6124532039e9f623549f5677af762260
Mustafa Ehsan Alokozay <mustafa.aloko@gmail.com>
2017-02-07 20:09:41 +0430

    Refactor

--- src/PashtoCardinal.php
+++ src/PashtoCardinal.php
@@ -25,42 +25,43 @@ class PashtoCardinal
             return '';
         }
 
-        $numberStr = (string) $this->number;
-        $numberList = array_reverse(str_split($numberStr));
+        $numberAsString = (string) $this->number;
+        $numberAsArray = array_reverse(str_split($numberAsString));
+        $numberLength = $this->getNumLength($this->number);
 
-        if ($this->getNumLength($this->number) == 1) {
+        if ($numberLength == 1) {
             return static::ONES[$this->number - 1];
-        } else if ($this->getNumLength($this->number) == 2) {
-            if ($numberList[1] == '1') {
-                return static::TENS_SMALLER_THAN_TWENTYONE[(int) $numberList[0] - 1];
-            } else if ($numberList[1] == '2' && $numberList[0] == '0') {
+        } else if ($numberLength == 2) {
+            if ($numberAsArray[1] == '1') {
+                return static::TENS_SMALLER_THAN_TWENTYONE[(int) $numberAsArray[0] - 1];
+            } else if ($numberAsArray[1] == '2' && $numberAsArray[0] == '0') {
                 return 'Shel';
             } else {
-                $oneField = $numberList[0] == '0' ? '' : static::ONES[$numberList[0] - 1] . ' ';
+                $oneField = $numberAsArray[0] == '0' ? '' : static::ONES[$numberAsArray[0] - 1] . ' ';
 
-                if ($numberList[1] != '2') {
-                    return $oneField . static::TENS[$numberList[1] - 1];
+                if ($numberAsArray[1] != '2') {
+                    return $oneField . static::TENS[$numberAsArray[1] - 1];
                 } else {
                     return $oneField . 'Wisht';
                 }
             }
-        } else if ($this->getNumLength($this->number) == 3) {
+        } else if ($numberLength == 3) {
             $tensNum = substr((string) $this->number, -2);
             $tensText = (new self((int) $tensNum))->convertToText();
 
-            if ($numberList[2] == 1) {
+            if ($numberAsArray[2] == 1) {
                 if ($tensText == '') {
                     return 'Sel';
                 }
 
                 return 'Yaw Salo ' . $tensText;
             } else {
-                return trim(static::ONES[$numberList[2] - 1] . ' Sawa ' . $tensText);
+                return trim(static::ONES[$numberAsArray[2] - 1] . ' Sawa ' . $tensText);
             }
-        }  else if ($this->getNumLength($this->number) > 3) {
+        }  else if ($numberLength > 3) {
             $numbersChunked = array_map(function($num) {
                 return strrev($num);
-            }, str_split(strrev($numberStr), 3));
+            }, str_split(strrev($numberAsString), 3));
 
             $numbersChunked = array_reverse($numbersChunked);
 
